3760c3fa1748f64339e31e10d11f44669814c810
christianesperar <christianesperar@gmail.com>
2018-08-22 01:51:36 +0800

    Sidebar fixes and enhancements - Fix menu item cannot hide after shown - Fix desktop collapse menu becomes hidden when you hover on it - No need to define arrow class inside the HTML

--- dist/assets/js/custom.js
+++ dist/assets/js/custom.js
@@ -22,6 +22,114 @@
 'use strict';
 
 (function ($) {
+  var $SIDEBAR = $('.c-sidebar');
+  var $MENU_ITEM = $SIDEBAR.find('.c-sidebar__menu-item');
+  var $MENU_LINK = $SIDEBAR.find('.c-sidebar__menu-link');
+  var $PARENT_MENU_ITEM = $SIDEBAR.find('.c-sidebar__menu > .c-sidebar__menu-list > .c-sidebar__menu-item');
+
+  var isSidebarCollapse = function isSidebarCollapse() {
+    return $SIDEBAR.hasClass('c-sidebar--collapse');
+  };
+  var resizeContent = function resizeContent() {
+    return window.ClnHelper.resizeContent();
+  };
+
+  /**
+   * Search for the current link and add `active` and `selected` class to parent menu item
+   * Please note that this is ideally handle by server side rendering
+   */
+  $MENU_LINK.filter(function (index, element) {
+    return element.href === window.location.href.split('#')[0].split('?')[0];
+  }).parents('.c-sidebar__menu-item:eq(1), .c-sidebar__menu-item:eq(2)').addClass('c-sidebar__menu-item--active c-sidebar__menu-item--selected');
+
+  // Add arrow if multilevel menu
+  $MENU_ITEM.has('.c-sidebar__menu-list').addClass('c-sidebar__menu-item--arrow');
+
+  $MENU_ITEM.on('click', function (e) {
+    e.stopPropagation();
+
+    var $element = $(e.currentTarget);
+    var $submenu = $element.children('.c-sidebar__menu-list');
+
+    if (!$submenu.length) return;
+
+    var isParentMenuItem = $element.parents(':eq(1)').hasClass('c-sidebar__menu');
+
+    if ($submenu.is(':visible')) {
+      if (isSidebarCollapse() && isParentMenuItem) return;
+
+      $element.removeClass('c-sidebar__menu-item--active');
+
+      $element.children('.c-sidebar__menu-list').slideUp();
+
+      $submenu.slideUp(resizeContent);
+    } else {
+      if (!isSidebarCollapse() && isParentMenuItem) {
+        $MENU_ITEM.removeClass('c-sidebar__menu-item--active');
+        $MENU_ITEM.children('.c-sidebar__menu-list').slideUp(resizeContent);
+      }
+
+      $element.addClass('c-sidebar__menu-item--active');
+
+      if (!isSidebarCollapse() || !isParentMenuItem) {
+        $submenu.slideDown(resizeContent);
+      }
+    }
+  });
+
+  /**
+   * Collapse and mobile
+   * Store current event to `bubbling` to fix issue where `mouseenter` and `click`
+   * are both triggered on mobile
+   */
+  var bubbling = void 0;
+
+  $PARENT_MENU_ITEM.on('mouseenter click', function (e) {
+    e.stopPropagation();
+
+    if (!isSidebarCollapse()) return;
+
+    clearTimeout(bubbling);
+
+    bubbling = setTimeout(function () {
+      var $element = $(e.currentTarget);
+
+      if ($element.hasClass('c-sidebar__menu-item--toggle')) {
+        $element.removeClass('c-sidebar__menu-item--toggle');
+      } else {
+        var $submenu = $element.children('.c-sidebar__menu-list');
+        var $SIDEBAR_MENU_ITEM_ACTIVE = $PARENT_MENU_ITEM.filter('.c-sidebar__menu-item--active').not($element);
+
+        if ($SIDEBAR_MENU_ITEM_ACTIVE.length) {
+          $MENU_ITEM.removeClass('c-sidebar__menu-item--active  c-sidebar__menu-item--toggle');
+          $MENU_ITEM.children('.c-sidebar__menu-list').css('display', 'none');
+        }
+
+        $element.addClass('c-sidebar__menu-item--active c-sidebar__menu-item--toggle');
+
+        if ($submenu.length) {
+          $submenu.css('display', 'block');
+        }
+      }
+    });
+  });
+
+  $PARENT_MENU_ITEM.on('mouseleave', function (e) {
+    if (!isSidebarCollapse()) return;
+
+    $(e.currentTarget).removeClass('c-sidebar__menu-item--toggle');
+  });
+
+  /**
+   * Resize content on load and when resizing window
+   */
+  resizeContent();
+
+  $(window).resize(resizeContent);
+})(jQuery);
+'use strict';
+
+(function ($) {
   var $CONTAINER = $('.c-container');
   var $SIDEBAR = $('.c-sidebar');
   var $SIDEBAR_TOGGLE = $('.c-header__sidebar-toggle');
@@ -32,82 +140,6 @@
     $CONTAINER.toggleClass('c-container--collapse');
     $FOOTER.toggleClass('c-footer--collapse');
 
-    window.ClnHelper.resizeContent();
-  });
-})(jQuery);
-'use strict';
-
-(function ($) {
-  var $SIDEBAR = $('.c-sidebar');
-  var $MENU_ITEM = $SIDEBAR.find('.c-sidebar__menu-item');
-  var $MENU_LINK = $SIDEBAR.find('.c-sidebar__menu-link');
-  var $PARENT_MENU_ITEM = $SIDEBAR.find('.c-sidebar__menu > .c-sidebar__menu-list > .c-sidebar__menu-item');
-
-  window.ClnHelper.resizeContent();
-
-  // Search for the current link and add active and selected class to parent menu item
-  $MENU_LINK.filter(function (index, element) {
-    return element.href === window.location.href.split('#')[0].split('?')[0];
-  }).parents(':eq(2), :eq(4)').addClass('c-sidebar__menu-item--active c-sidebar__menu-item--selected');
-
-  $MENU_ITEM.on('click', function (e) {
-    var $element = $(e.currentTarget);
-    var $submenu = $element.children('.c-sidebar__menu-list');
-
-    if (!$submenu.length) {
-      e.stopPropagation();
-      return;
-    }
-
-    var isParentMenuItem = $element.parents(':eq(1)').hasClass('c-sidebar__menu');
-
-    if ($submenu.is(':visible')) {
-      var isSidebarCollapse = $SIDEBAR.hasClass('c-sidebar--collapse');
-
-      if (isSidebarCollapse && isParentMenuItem) return;
-
-      $element.removeClass('c-sidebar__menu-item--active');
-
-      $element.find('.c-sidebar__menu-list').slideUp();
-
-      $submenu.slideUp(function () {
-        window.ClnHelper.resizeContent();
-      });
-    } else {
-      if (isParentMenuItem) {
-        $MENU_ITEM.removeClass('c-sidebar__menu-item--active');
-      }
-
-      $element.addClass('c-sidebar__menu-item--active');
-
-      $submenu.slideDown(function () {
-        window.ClnHelper.resizeContent();
-      });
-    }
-
-    e.stopPropagation();
-  });
-
-  // Collapse
-  $PARENT_MENU_ITEM.on('mouseenter', function (e) {
-    var $element = $(e.currentTarget);
-
-    if (!$SIDEBAR.hasClass('c-sidebar--collapse') || $element.hasClass('c-sidebar__menu-item--active')) return;
-
-    var $submenu = $element.children('.c-sidebar__menu-list');
-    var $SIDEBAR_MENU_ITEM_ACTIVE = $SIDEBAR.find('.c-sidebar__menu-item--active');
-
-    $SIDEBAR_MENU_ITEM_ACTIVE.removeClass('c-sidebar__menu-item--active');
-    $SIDEBAR_MENU_ITEM_ACTIVE.find('.c-sidebar__menu-list').css('display', 'none');
-
-    $element.addClass('c-sidebar__menu-item--active');
-
-    if ($submenu.length) {
-      $submenu.css('display', 'block');
-    }
-  });
-
-  $(window).resize(function () {
     window.ClnHelper.resizeContent();
   });
 })(jQuery);
